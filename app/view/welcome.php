faa6d7e077f18f11df9ea7883f5d92122a4727b9
Corey Losenegger <corey@coreyloose.com>
2011-03-22 09:36:58 -0500

    Updating default welcome page to have correct url

--- app/view/welcome.php
+++ app/view/welcome.php
@@ -1,7 +1,8 @@
 <h1>Welcome To Capsule</h1>
 
+<p>Now that you've got the framework setup it is time to get to work.</p>
+
 <p>
-Now that you've got the framework setup it is time to get to work. If you've
-got any questions or comments along the way you can stop by
-<a href="http://capsule.coreyloose.com">Capsule Headquarters</a>.
+If you've got any questions or comments along the way you can stop by
+<a href="http://capsulephp.org">Capsule Headquarters</a>.
 </p>
